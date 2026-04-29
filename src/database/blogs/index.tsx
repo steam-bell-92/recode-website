958f2077dbae8bd7383668e65f7dc85e90db050b
sanjay-kv <sanjay.kanakkot@gmail.com>
2026-04-29 11:07:52 +1000

    changed plag blog posts

--- src/database/blogs/index.tsx
+++ src/database/blogs/index.tsx
@@ -10,18 +10,7 @@ interface Blog {
 }
 
 const blogs: Blog[] = [
-  {
-    id: 1,
-    title: "Streamline Your UX Design",
-    image: "/img/blogs/05-ux-streamline.png",
-    description:
-      "User experience design can be overwhelming because of the number of factors that influence what a product should look like and how it should function.",
-    slug: "streamline-ux-ui",
-    authors: ["sowmiya-v", "sanjay-kv"],
-    category: "Design",
-    tags: ["UX", "UI", "Design", "User Experience"],
-  },
-
+ 
   {
     id: 2,
     title: "Land a Job in UI/UX Design",
@@ -32,39 +21,6 @@ const blogs: Blog[] = [
     authors: ["sowmiya-v", "sanjay-kv"],
     category: "Design",
     tags: ["UX", "UI", "Career", "Job"],
-  },
-  {
-    id: 3,
-    title: "UX Designers Future with AI",
-    image: "/img/blogs/03-ui-ux.png",
-    description:
-      "The impact of technology on UX design is undeniable. Automation and artificial intelligence are making it easier to identify user needs and create tailored experiences.",
-    slug: "ux-designers-ai",
-    authors: ["sowmiya-v", "sanjay-kv"],
-    category: "AI & Tech",
-    tags: ["AI", "UX", "Design", "Technology"],
-  },
-  {
-    id: 4,
-    title: "What is Google DeepMind?",
-    image: "/img/blogs/02-deepmind.png",
-    description:
-      "DeepMind is an auxiliary of Google that centers around man-made brainpower. It utilizes a part of AI called AI",
-    slug: "google-deepmind",
-    authors: ["sowmiya-v", "sanjay-kv"],
-    category: "AI & Tech",
-    tags: ["AI", "Google", "DeepMind", "Machine Learning"],
-  },
-  {
-    id: 5,
-    title: "What are backlinks for SEO",
-    image: "/img/blogs/01-seo-image.png",
-    description:
-      "An SEO backlink is created when one website links to another, and they're extremely important for SEO. ",
-    slug: "google-backlinks",
-    authors: ["sanjay-kv"],
-    category: "Resources",
-    tags: ["SEO", "Marketing", "Backlinks", "Web Development"],
   },
 
   {
